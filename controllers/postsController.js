f398eee01ec1772b1c381ed27940fece4e9ca55f
trongtuan99er <trongtuan681@gmail.com>
2023-03-22 20:00:59 +0700

    create new post API

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -16,3 +16,23 @@ export const getPosts = (req, res) => {
   })
 }
 
+export const addPosts = (req, res) => {
+  const token = req.cookies.accessToken
+  if(!token) return res.status(400).json("Chưa đăng nhập!")
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
+    if(err) return res.status(403).json("token sai")
+    const q = "INSERT INTO posts (`desc`, `img`, `createdAt`, `userId`) VALUES (?)"
+
+    const values = [
+      req.body.desc,
+      req.body.img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+      data.id
+    ]
+    db.query(q,[values], (err, data) => {
+      if(err) return res.status(500).json(err)
+      return res.status(200).json("Tạo thành công!")
+    })
+  })
+}
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 import express from "express";
-import {  } from '../controllers/postsController.js'
+import { getPosts, addPosts } from '../controllers/postsController.js'
 const router = express.Router()
 
 router.get("/", getPosts)
+router.post("/", addPosts)
 export default router
