c79a6decb1cc8b5c6460b10c3545438e23d0776a
trongtuan99er <trongtuan681@gmail.com>
2023-03-22 21:15:10 +0700

    add comment endpoint API

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { getComments } from '../controllers/commentsController.js'
+import { getComments, addComment } from '../controllers/commentsController.js'
 const router = express.Router()
 
 router.get('/', getComments)
+router.post('/', addComment)
 export default router
