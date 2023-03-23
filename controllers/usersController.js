722568ae611936814be469470f2a2b7fe3eafd1a
trongtuan99er <trongtuan681@gmail.com>
2023-03-23 18:48:20 +0700

    get profile info API

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,12 @@
-export const getUser = (req,res) => {
-  res.send('working ne')
+import { db } from "../connect.js";
+import jwt from 'jsonwebtoken'
+
+export const getUser = (req, res) => {
+  const q = `SELECT * FROM users WHERE id = ?`
+
+  db.query(q,[req.params.userId], (err, data) => {
+    if(err) return res.status(500).json(err)
+    const { password, ...info } = data[0]
+    return res.status(200).json(info)
+  })
 }
