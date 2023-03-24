878b2a805a18d307b54c8d6a74d62a517de9e8bb
trongtuan99er <trongtuan681@gmail.com>
2023-03-24 10:22:51 +0700

    handle get posts from wall and timeline

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -7,9 +7,12 @@ export const getPosts = (req, res) => {
   if(!token) return res.status(400).json("Chưa đăng nhập!")
   jwt.verify(token, process.env.SECRET_KEY, (err, data) => {
     if(err) return res.status(403).json("token sai")
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC`
-  
-    db.query(q,[data.id, data.id], (err, data) => {
+    const userId = req.query.userId
+
+    const q = userId !== 'undefined' ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC` :  `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId = ? OR p.userId = ? ORDER BY p.createdAt DESC`
+    
+    const values = userId !== 'undefined' ? [userId] : [data.id, data.id]
+    db.query(q,values,(err, data) => {
       if(err) return res.status(500).json(err)
       return res.status(200).json(data)
     })
