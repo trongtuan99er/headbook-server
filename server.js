88f9a59f4eb099e212cf8a10ffc243c73548ed91
trongtuan99er <trongtuan681@gmail.com>
2023-04-15 14:17:13 +0700

    congif sv be

--- server.js
+++ server.js
@@ -1,37 +1,35 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-import userRoutes from './routes/users.js'
-import postRoutes from './routes/posts.js'
-import likeRoutes from './routes/likes.js'
-import authRoutes from './routes/auth.js'
-import commentRoutes from './routes/comments.js'
-import relationshipRoutes from './routes/relationships.js'
-import cors from 'cors'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import likeRoutes from "./routes/likes.js";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import relationshipRoutes from "./routes/relationships.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
 // middlewares
-app.use((req,res,next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", true);
-  next()
-}) 
-app.use(express.json())
-app.use(cors({
-  origin: "https://headbook-ui.netlify.app"
-}))
-app.use(cookieParser())
+  next();
+});
+app.use(express.json());
+app.use(cors({}));
+app.use(cookieParser());
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/posts', postRoutes)
-app.use('/api/likes', likeRoutes)
-app.use('/api/comments', commentRoutes)
-app.use('/api/relationships', relationshipRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
 app.listen(port, () => {
   console.log(`server start at port: ${port}`);
-})
+});
